0d50319189768479aff21d161b7af57bfe7f6106
kello22e <kello22e@mtholyoke.edu>
2018-06-02 18:21:08 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -43,8 +43,8 @@ def turn(board)
   input = gets.strip
   index = input_to_index(input)
   
-  if valid_move?(board, input.to_i)
-    move(board,input.to_i,"X")
+  if valid_move?(board, index)
+    move(board,index,"X")
   else
     turn(board) and return
   end
